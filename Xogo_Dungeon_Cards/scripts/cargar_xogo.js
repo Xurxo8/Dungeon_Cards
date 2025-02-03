69c5f284318d392ff216cd616230952ed2637ab8
Xurxo Freitas <152525151+Xurxo8@users.noreply.github.com>
2025-02-03 13:30:37 +0100

    Engadido acceso a base de datos
    Ahora xa cargamos os datos dende a base de datos, tamen se mellorou a interface do menú inicial

--- Xogo_Dungeon_Cards/scripts/cargar_xogo.js
+++ Xogo_Dungeon_Cards/scripts/cargar_xogo.js
@@ -1,22 +1,22 @@
 // Cargar pantalla de inicio
 function cargarMenu() {
-  document.body.innerHTML = `
-    <div id="inicio">
+	document.body.innerHTML = `
+		<div id="inicio">
 		<div id="titulo"><img src="imaxes/DungeonCardsPressImages/Logo/Logo3.png" alt="logo"></div>
 		<div id="menu">
-			<div id="seleccionPersonaxe"><img src="imaxes/DungeonCardsPressImages/Characters/Knight.png" alt="personaxe seleccionado"></div>
-			<!-- <div id="seleccionPoderes">
-				<div id="poder_1" class="poderSeleccionado">Poder 1</div>
-				<div id="poder_2" class="poderSeleccionado">Poder 2</div>
-				<div id="poder_3" class="poderSeleccionado">Poder 3</div>
-			</div> -->
-			<!-- <div id="logros">trofeo</div> -->
+			<!-- <div id="seleccionPersonaxe"><img src="imaxes/DungeonCardsPressImages/Characters/Knight.png" alt="personaxe seleccionado"></div>
 			<div id="xogar">
 				<p onclick="cargarMazmorra()"><img src="imaxes/logos/boton_xogar.svg" alt="boton xogar"></p>
-			</div>
+			</div> -->
+
+			<ul>
+				<li id="seleccionPersonaxe">Seleccionar personaxe</li>
+				<li id="cargarClasificacion">Taboa clasificatoria</li>
+				<li id="xogar" onclick="cargarMazmorra()">Comezar</li>
+			</ul>
 		</div>
 	</div>
-  `;
+	`;
 }
 
 // Cargar a mazmorra
@@ -34,7 +34,7 @@ function cargarMazmorra() {
 				<div id="sair">
 					<p onclick="cargarMenu()"><img src="./imaxes/atras_logo.png" alt="atras_logo"></p>
 				</div>
-				<div class="itemActual">
+				<div id="itemActual">
 					<img src="./imaxes/cartas/espada.png" alt="espada.png">
 					<img id="frecha" src="./imaxes/frecha.png" alt="frecha.png">
 					<img src="./imaxes/cartas/moeda.png" alt="moeda.png">
@@ -45,7 +45,7 @@ function cargarMazmorra() {
 	`;
 
 	cargarCuadricula();
-  tutorial();
+	tutorial();
 }
 
 // Xerar os items e personaxes do taboleiro
@@ -53,41 +53,49 @@ function cargarCuadricula() {
 	const NUM_CELAS = 9;
 	const VIDA_MAXIMA = 10; // Puntos de saude maxima do personaxe
 	let puntosVida = 10; // Vida do xogador
-	let vidaMonstro;
 	let danoArma = 10;
 
-	for (let i = 0; i < NUM_CELAS; i++) {
-		if(i != 4){ // Na posición central da cuadricula poñemos ao xogador
-			cuadricula.innerHTML += `
-			<div class="cela">
-				<div class="puntosVida">
-					<label>${puntosVida}</label>
-					<img src="./imaxes/corazon.png" alt="">
+	$.getJSON('./servidor/cargarCartas.php')
+	.done(function(cartas){
+		console.log(cartas);
+		for (let i = 0; i < NUM_CELAS; i++) {
+			if(i != 4){ // Na posición central da cuadricula poñemos ao xogador
+				cuadricula.innerHTML += `
+				<div class="cela">
+					<div class="puntosVida">
+						<label>${cartas[i].vida}</label>
+						<img src="./imaxes/corazon.png" alt="">
+					</div>
+					<img src="./imaxes/cartas/morcego.png" alt="">
+					<p class="nomeItem">Morcego</p>
+					<div class="cantidade"></div>
 				</div>
-				<img src="./imaxes/cartas/morcego.png" alt="">
-				<p class="nomeItem">Morcego</p>
-				<div class="cantidade"></div>
-			</div>
-		`;
-		}else{
-			cuadricula.innerHTML += `
-			<div class="cela" id="xogador">
-		 		<div id="arma">
-		 			<img src="./imaxes/cartas/espada.png" alt="">
-		 		</div>
-		 		<div class="puntosVida">
-		 			<label>${puntosVida}/${VIDA_MAXIMA}</label>
-		 			<img src="./imaxes/corazon.png" alt="">
-		 		</div>
-		 		<img src="./imaxes/personaxes/cabaleiro.png" alt="">
-		 		<p class="nomeItem">Heroe</p>
-		 		<div class="cantidade">${danoArma}</div>
-		 	</div>
-		`;
+			`;
+			}else{
+				cuadricula.innerHTML += `
+				<div class="cela" id="xogador">
+					<div id="arma">
+						<img src="./imaxes/cartas/espada.png" alt="">
+					</div>
+					<div class="puntosVida">
+						<label>${puntosVida}/${VIDA_MAXIMA}</label>
+						<img src="./imaxes/corazon.png" alt="">
+					</div>
+					<img src="./imaxes/personaxes/cabaleiro.png" alt="">
+					<p class="nomeItem">Heroe</p>
+					<div class="cantidade">${danoArma}</div>
+				</div>
+			`;
+			}
 		}
-	}
+	})
+	.fail(function(){
+		alert('Erro ao cargar as cartas');
+	})
 }
 
 function tutorial(){
-  alert(" Para mover o personaxe utiliza as frechas do teclado")
+	alert(" Para mover o personaxe utiliza as frechas do teclado")
 }
+
+
