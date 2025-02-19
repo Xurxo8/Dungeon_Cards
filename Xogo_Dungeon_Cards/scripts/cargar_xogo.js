757d737578d8a0b81bc7620a472f75b74a7dbf53
Xurxo Freitas <152525151+Xurxo8@users.noreply.github.com>
2025-02-19 11:49:01 +0100

    Creación dun map() - NON REMATADO
    En vez de usar grid 100% utilicerei map para gardar as coordenadas das celdas

--- Xogo_Dungeon_Cards/scripts/cargar_xogo.js
+++ Xogo_Dungeon_Cards/scripts/cargar_xogo.js
@@ -1,4 +1,6 @@
-// Cargar pantalla de inicio
+// ==================================================================
+// ========== CARGAR PANTALLA DE INICIO ==========
+// ==================================================================
 function cargarMenu() {
 	document.body.innerHTML = `
 		<div id="inicio">
@@ -18,7 +20,9 @@ function cargarMenu() {
 	`;
 }
 
-// Cargar a mazmorra
+// ==================================================================
+// ========== CARGAR MAZMORRA ==========
+// ==================================================================
 function cargarMazmorra() {
 	document.body.innerHTML = `
 		<div id="taboleiro">
@@ -43,24 +47,64 @@ function cargarMazmorra() {
 		<div id="version">v0.0.0</div>
 	`;
 
-	cargarCuadricula();
-	tutorial();
+	// cargarCuadricula();
+	renderGrid();
+	// tutorial();
 }
 
-// Xerar os items e personaxes do taboleiro
+// ==================================================================
+// ========== XERAR OS ITEMS DO TABOLEIRO ==========
+// ==================================================================
+// Crear o map() para gardar as posicions das celdas
+// INFO Map y set JS -> https://es.javascript.info/map-set
+let taboleiro = new Map();
+
+const FILAS = 3;
+const COLUMNAS = 3;
+
+for (let f = 1; f <= FILAS; f++) {
+	for (let c = 1; c <= COLUMNAS; c++) {
+		taboleiro.set(`${f},${c}`, null); // Creamos o mapa valeiro
+	}	
+}
+
+console.log(taboleiro);
+
+// Array do que cargaremos os items ou inimigos
+// let mazo = [cargarInimigos, cargarArmas, cargarObxectos];
+
+// Colocamos o personaxe no centro do taboleiro
+taboleiro.set('2,2', "👾");
+
+console.log(taboleiro);
+
+
+// Renderizar a grilla no DOM
+function renderGrid(){
+	$('#cuadricula').html(""); // Limpar taboleiro
+
+	for (let f = 1; f <= FILAS; f++) {
+		for(let c = 1; c <= COLUMNAS; c++){
+			taboleiro.get(`${f},${c}`) || "🟨";
+		}		
+	}
+}
+
+console.log(taboleiro);
+
 function cargarCuadricula() {
 	const NUM_CELAS = 9;
+	let centro = Math.trunc(NUM_CELAS / 2);
 	const VIDA_MAXIMA = 10; // Puntos de saude maxima do personaxe
 	let puntosVida = 10; // Vida do xogador
 	let danoArma = 10;
 
 	$.getJSON('./servidor/cargarInimigos.php')
 	.done(function(inimigos){
-		console.log(inimigos);
 		for (let i = 0; i < NUM_CELAS; i++) {
-			if(i != 4){ // Na posición central da cuadricula poñemos ao xogador
+			if(i != centro){ // Na posición central da cuadricula poñemos ao xogador
 				cuadricula.innerHTML += `
-				<div class="cela">
+				<div id="cela${i}" class="cela">
 					<div class="puntosVida">
 						<label>${inimigos[i].vida}</label>
 						<img src="./imaxes/corazon.png" alt="">
@@ -89,12 +133,51 @@ function cargarCuadricula() {
 		}
 	})
 	.fail(function(erro){
-		alert('Erro ao cargar entrar na mazmorra');
-	})
+		alert('Erro ao entrar na mazmorra');
+		console.log(erro);
+	});
 }
+
 
 function tutorial(){
 	alert(" Para mover o personaxe utiliza as frechas do teclado")
 }
 
+// ==================================================================
+// ========== FUNCION PARA CARGAR INIMIGOS ==========
+// ==================================================================
+// function cargarInimigos(){
+// 	$.getJSON('./servidor/cargarInimigos.php')
+// 		.done(function(inimigos){
+// 			console.log("Inimigos ben");
+// 		})
+// 		.fail(function(erro){
+// 			alert("ERRO ao cargar inimigos");
+// 		})
+// }
 
+// ==================================================================
+// ========== FUNCION PARA CARGAR OBXETOS ==========
+// ==================================================================
+// function cargarObxectos(){
+// 	$.getJSON('./servidor/cargarObxectos.php')
+// 		.done(function(inimigos){
+// 			console.log("Obxectos ben");
+// 		})
+// 		.fail(function(erro){
+// 			alert("ERRO ao cargar obxetos");
+// 		})
+// }
+
+// ==================================================================
+// ========== FUNCION PARA CARGAR ARMA ==========
+// ==================================================================
+// function cargarArmas(){
+// 	$.getJSON('./servidor/cargarArma.php')
+// 		.done(function(inimigos){
+// 			console.log("Armas ben");
+// 		})
+// 		.fail(function(erro){
+// 			alert("ERRO ao cargar armamento");
+// 		})
+// }
