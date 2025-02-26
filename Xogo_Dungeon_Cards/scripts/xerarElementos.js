1b302068857517f4f378e4dea92856b76982a59c
Xurxo Freitas <152525151+Xurxo8@users.noreply.github.com>
2025-02-26 12:25:27 +0100

    Delete Xogo_Dungeon_Cards/scripts/xerarElementos.js
    Non necesario

--- Xogo_Dungeon_Cards/scripts/xerarElementos.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$.getJSON('./servidor/cargarCartas.php')
-		.done(function(erro){
-			
-		})
-		.fail(function(erro){
-			alert('Problemas ao cargar as cartas');
-		});
-});
